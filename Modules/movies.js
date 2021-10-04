94189017fbad87051f4e09f06e7bbeff166db656
Saif-K-Saeed <saif.k.saeed@gmail.com>
2021-10-04 12:40:15 +0300

    fix movie

--- Modules/movies.js
+++ Modules/movies.js
@@ -1,6 +1,6 @@
-
+'use strict';
 const axios = require('axios');
-let cacheMemory ={};
+
 // Class --------------------------------------------//
 class Movie {
   constructor(element) {
@@ -11,31 +11,21 @@ class Movie {
     this.image_url = 'https://image.tmdb.org/t/p/w500' + element.poster_path;
     this.popularity = element.popularity;
     this.released_on = element.release_date;
-
   }
 }
-// localhost:3001/getMovie?query=<name Of MOvie>
+function randomMoviePage() {
+  return Math.floor((Math.random() * 20) + 1);
+}
+// localhost:3001/getMovie
 function getMovieHandler(req, res) {
-  let {query} = req.query;
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`;
-  if (cacheMemory[query] !== undefined) {
-    res.send(cacheMemory[query]);
-  }
-  else {
-    try {
-      axios.get(url).then(results => {
-
-        let newMovieArray = results.data.results.map(element => {
-          return new Movie(element);
-        });
-        cacheMemory[query] = newMovieArray;
-        res.send(newMovieArray);
-      });
-    }
-    catch (error) {
-      res.send(error);
-    }
-  }
-
+  let newObjectArray = [];
+  let rand = randomMoviePage();
+  let URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=${rand}`;
+  axios.get(URL).then(element => {
+    element.data.results.forEach(ind => {
+      newObjectArray.push(new Movie(ind))
+    });
+    res.send(newObjectArray.slice(0, 6));
+  })
 }
 module.exports = { getMovieHandler };
